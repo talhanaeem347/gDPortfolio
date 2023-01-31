e5fef33c5e3dad5d8476235d36b44e0af127a866
Talha Naeem <talhanaeem347@gmail.com>
2023-01-31 17:18:50 +0500

    text justify

--- src/components/Services.js
+++ src/components/Services.js
@@ -9,14 +9,14 @@ section.innerHTML = `
       <div class="w-full lg:w-1/2 md:w-1/2 sm:w-1/2 mb-8 p-2  flex flex-col">
         <img src="/assets/images/coding.png" alt="Web Design" class="h-96 ">
         <h2 class="text-xl font-bold text-gray-800 m-4">Web Design</h2>
-        <p class="text-lg text-gray-600 text-center">
+        <p class="text-lg text-gray-600 text-justify">
         I specialize in creating dynamic, user-friendly websites that deliver an exceptional user experience.
         </p>
       </div>
       <div class="w-full lg:w-1/2 md:w-1/2 sm:w-1/2 h-2/3 mb-8 p-2 flex flex-col">
         <img src="/assets/images/eagleEagle.png" alt="Logo Design" class="h-96 ">
         <h2 class="text-xl font-bold text-gray-800 m-4">Logo Design</h2>
-        <p class="text-lg text-gray-600 text-center">
+        <p class="text-lg text-gray-600 text-justify">
         I offer professional logo design services to help bring your brand to life.
         </p>
       </div>
